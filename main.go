28a55d0d917e2b64cbe51afeaf6d3796fbbdad15
Paul Maddox <paul.maddox@ingg.com>
2014-08-28 15:24:35 +0100

    Use $DOCKER_HOST env variable to connect to docker, if it's available.

--- main.go
+++ main.go
@@ -4,23 +4,31 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/GeertJohan/go.rice"
 	"github.com/fsouza/go-dockerclient"
 	"github.com/ogier/pflag"
 )
 
-var _bind = pflag.StringP("bind", "b", "0.0.0.0", "Bind address to listen on")
-var _port = pflag.IntP("port", "p", 3000, "HTTP port to listen on")
-var _docker = pflag.StringP("docker", "d", "unix:///var/run/docker.sock", "Docker server address")
-
 func main() {
+
+	var _bind = pflag.StringP("bind", "b", "0.0.0.0", "Bind address to listen on")
+	var _port = pflag.IntP("port", "p", 3000, "HTTP port to listen on")
+	var _docker *string
+
+	// If $DOCKER_HOST env variable is set, use that to connect to docker
+	// otherwise resort to the default unix socket.
+	if os.Getenv("DOCKER_HOST") != "" {
+		_docker = pflag.StringP("docker", "d", os.Getenv("DOCKER_HOST"), "Docker server address")
+	} else {
+		_docker = pflag.StringP("docker", "d", "unix:///var/run/docker.sock", "Docker server address")
+	}
 
 	// Parse the CLI arguments
 	pflag.Parse()
 
 	log.Printf("Connecting to docker at %s", *_docker)
-
 	client, err := docker.NewClient(*_docker)
 	if err != nil {
 		log.Printf("Failed to connect to docker (%s)", err)
